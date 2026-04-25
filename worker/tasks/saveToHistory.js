f42e546e3594bbfeca3feb329bf2fade4481c251
emagu <emagudev@gmail.com>
2026-04-25 21:32:33 +0800

    修正每十分鐘保存排程錯誤

--- worker/tasks/saveToHistory.js
+++ worker/tasks/saveToHistory.js
@@ -2,8 +2,8 @@
 export async function saveToHistoryTask(env) {
     try {
         await env.DB.prepare(
-        `INSERT INTO device_history (device_id, temperature, light, post_at) SELECT device_id, avgTemp, avgLight, datetime('now') FROM device_status WHERE device_id = ?`
-      ).bind(deviceId).run();
+        `INSERT INTO device_history (device_id, temperature, light, post_at) SELECT device_id, avgTemp, avgLight, datetime('now') FROM device_status`
+      ).run();
         console.log(`save to history data successfully at ${new Date().toISOString()}`);
     } catch (error) {
         console.error("Save to history error:", error);
